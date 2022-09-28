48ec6695c36255ccf5923ba214b3a30f1504961d
luther <chapman.gomez.richard@gmail.com>
2022-09-28 19:02:46 -0400

    note command added  - multiline stdin read  - exit flag for note (--end)  - clean string after finish
    TODO
     - create multiline read as a micro service of the program

--- src/commands/note.rs
+++ src/commands/note.rs
@@ -1,5 +1,6 @@
 use crate::models::commands_model::{Command};
-use crate::read::read_line::{use_default_prompt};
+use crate::read::read_line::{use_default_prompt, prompt};
+use crate::utils::sanitation;
 
 pub fn create_note() ->  Command<'static> {
     Command::new("note", &root, None)
@@ -11,6 +12,35 @@ fn root(_cmd: &Command) {
     
     let reply = use_default_prompt("Local or remote note? (local/remote)");
 
-    println!("Your answer {}", reply);
+    let mut text = String::new();
+    let mut my_note = String::new();
 
+    println!("End note with --end");
+
+    loop {
+        std::io::stdin().read_line(&mut text).expect("Error from STDIN");
+
+        my_note.push_str(&text);
+        
+        sanitation::clean_string(&mut text);     
+        
+        if text.chars().count() >= 5 {
+            let count = text.chars().count() - 5;
+            text.drain(..count);
+        }
+
+        // println!(" from drain \n{text}\n");
+
+        if  text == "--end" {
+            break;
+        };
+
+        text = String::new();
+    }
+
+    let my_note: String = my_note.drain(..(my_note.chars().count() - 6)).collect();
+
+    println!("\n\n");
+
+    println!("{my_note}");
 }
