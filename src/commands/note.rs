08a96a19a6653d94a7d927a69e21de9fecd3ff19
luther <chapman.gomez.richard@gmail.com>
2022-09-28 21:47:49 -0400

    note command   - saved to local folder   - allows for rewrite of note
    TODO
     sub commands
      - list notes
      - delete note
      - edit note

--- src/commands/note.rs
+++ src/commands/note.rs
@@ -8,21 +8,73 @@ pub fn create_note() ->  Command<'static> {
 
 
 
+
 fn root(_cmd: &Command) {
+    note_interface(true);
+}
+
+fn note_interface(first: bool) {
     
-    let reply = use_default_prompt("Local or remote note? (local/remote)");
+    if first == true {
+        let mut reply = use_default_prompt("Local or remote note? (local/remote)");
+        sanitation::clean_string(&mut reply);
+        if reply.to_lowercase() == "local" {
+            create_local_note_dir().expect("Error creating local dirctory");
+        } 
+    }
 
     let mut text = String::new();
     let mut my_note = String::new();
 
-    println!("End note with --end");
+    println!("End note with --end\n\n");
 
+    writing_interface(&mut text, &mut my_note);
+
+    println!("\n");
+
+    sanitation::clean_string(&mut my_note);
+
+    let my_note: String = my_note.drain(..(my_note.chars().count() - 5)).collect();
+
+    check_if_note_is_ready(&my_note);
+}
+
+
+/// Checks if the written note is ready
+/// 
+fn check_if_note_is_ready(note: &String) {
+    
+    println!("\"{}\"\n", note);
+    
+    let mut is_ready = use_default_prompt("Is the above note ready? (y/n)");
+
+    sanitation::clean_string(&mut is_ready);
+
+    if is_ready == "y" {
+        let mut note_name = use_default_prompt("Whats the name of the note?");
+
+        sanitation::clean_string(&mut note_name);
+
+        match save_note(&note_name, note) {
+            Ok(_rslt) => println!("Note have been saved"),
+            Err(error) => println!("Failed to save note, {}", error)
+        };
+        
+    }
+    else if is_ready == "n" {
+        note_interface(false);
+    }
+}
+ 
+
+/// Creates multiline read from STDIN
+fn writing_interface(text: &mut String, my_note: &mut String) {
     loop {
-        std::io::stdin().read_line(&mut text).expect("Error from STDIN");
+        std::io::stdin().read_line(text).expect("Error from STDIN");
 
         my_note.push_str(&text);
         
-        sanitation::clean_string(&mut text);     
+        sanitation::clean_string( text);     
         
         if text.chars().count() >= 5 {
             let count = text.chars().count() - 5;
@@ -35,12 +87,26 @@ fn root(_cmd: &Command) {
             break;
         };
 
-        text = String::new();
+        *text = String::new();
     }
+}
 
-    let my_note: String = my_note.drain(..(my_note.chars().count() - 6)).collect();
+/// Create local auto directore for notes
+/// 
+fn create_local_note_dir() -> Result<bool, std::io::Error> {
 
-    println!("\n\n");
+    std::fs::create_dir_all("./.auto/notes")?;
 
-    println!("{my_note}");
+    Ok(true)
+}
+
+fn save_note(note_name: &String, note_content: &String) -> Result<bool, std::io::Error> {
+
+    let path = format!("{}{}","./.auto/notes/", note_name);
+
+    println!("{path}");
+    
+    std::fs::write(path, note_content)?;
+
+    Ok(true)
 }
